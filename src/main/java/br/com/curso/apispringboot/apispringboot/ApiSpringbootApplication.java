00bc4158012b0119d10668b5650650c3b512098a
gabriel.negrao <gabriel.negrao@iteris.com.br>
2019-02-12 13:48:06 -0200

    Projeto criado

---
+++ src/main/java/br/com/curso/apispringboot/apispringboot/ApiSpringbootApplication.java
@@ -0,0 +1,14 @@
+package br.com.curso.apispringboot.apispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootApplication.class, args);
+	}
+
+}
+
