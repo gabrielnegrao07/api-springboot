c71eff7e361b8d3f45ca9c87c5d40e44c06b9d45
gabriel.negrao <gabriel.negrao@iteris.com.br>
2019-02-14 11:22:15 -0200

    Criando operação de instanciacao

--- src/main/java/br/com/curso/apispringboot/apispringboot/ApiSpringbootApplication.java
+++ src/main/java/br/com/curso/apispringboot/apispringboot/ApiSpringbootApplication.java
@@ -1,14 +1,32 @@
 package br.com.curso.apispringboot.apispringboot;
 
+import br.com.curso.apispringboot.apispringboot.domain.Categoria;
+import br.com.curso.apispringboot.apispringboot.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ApiSpringbootApplication {
+public class ApiSpringbootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSpringbootApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
 
