c23e697938f058b0113bb190547defc10238628e
gabriel.negrao <gabriel.negrao@iteris.com.br>
2019-02-27 15:00:09 -0300

    Subtotal e total do Pedido

--- src/main/java/br/com/curso/apispringboot/apispringboot/domain/Pedido.java
+++ src/main/java/br/com/curso/apispringboot/apispringboot/domain/Pedido.java
@@ -45,6 +45,14 @@ public class Pedido implements Serializable {
         this.enderecoEntrega = enderecoEntrega;
     }
 
+    public double getValorTotal(){
+        double soma = 0;
+        for (ItemPedido ip : itens){
+            soma = soma + ip.getSubTotal();
+        }
+        return soma;
+    }
+
     public Integer getId() {
         return id;
     }
