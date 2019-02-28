01c4f69237acf43935519fa0be9a584397e97fce
gabriel.negrao <gabriel.negrao@iteris.com.br>
2019-02-28 10:17:12 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/curso/apispringboot/apispringboot/config/DevConfig.java
@@ -0,0 +1,30 @@
+package br.com.curso.apispringboot.apispringboot.config;
+
+import br.com.curso.apispringboot.apispringboot.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!"create".equals(strategy)){
+            return false;
+        }
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
