341e61b687ba0779ea0e33450776c6e2a9e3bd9d
gabriel.negrao <gabriel.negrao@iteris.com.br>
2019-02-28 09:20:05 -0300

    Criando profile de teste

---
+++ src/main/java/br/com/curso/apispringboot/apispringboot/config/TestConfig.java
@@ -0,0 +1,23 @@
+package br.com.curso.apispringboot.apispringboot.config;
+
+import br.com.curso.apispringboot.apispringboot.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
