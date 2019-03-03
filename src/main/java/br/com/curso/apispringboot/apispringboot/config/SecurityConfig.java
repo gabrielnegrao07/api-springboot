6d9d03a5fb01e4f7a1ae4e5d211015066e9ee3c9
gabriel.negrao <gabriel.negrao@iteris.com.br>
2019-03-03 12:37:15 -0300

    Implementando autorizacao

--- src/main/java/br/com/curso/apispringboot/apispringboot/config/SecurityConfig.java
+++ src/main/java/br/com/curso/apispringboot/apispringboot/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package br.com.curso.apispringboot.apispringboot.config;
 
 import br.com.curso.apispringboot.apispringboot.security.JWTAuthenticationFilter;
+import br.com.curso.apispringboot.apispringboot.security.JWTAuthorizationFilter;
 import br.com.curso.apispringboot.apispringboot.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -52,6 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(),jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(),jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
