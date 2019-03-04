bc63d4f28459480256cf01c817be591b26673e02
gabriel.negrao <gabriel.negrao@iteris.com.br>
2019-03-03 21:06:44 -0300

    Refresh token

---
+++ src/main/java/br/com/curso/apispringboot/apispringboot/controller/AuthController.java
@@ -0,0 +1,28 @@
+package br.com.curso.apispringboot.apispringboot.controller;
+
+import br.com.curso.apispringboot.apispringboot.security.JWTUtil;
+import br.com.curso.apispringboot.apispringboot.security.UserSS;
+import br.com.curso.apispringboot.apispringboot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value="/refresh_token", method= RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+}
